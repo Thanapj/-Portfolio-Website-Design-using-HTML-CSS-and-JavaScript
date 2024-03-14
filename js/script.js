9fb71f57ef57557a7a5b2446c3e9a8a1498d54a3
Thanaphum Jutiya <157394194+Thanapj@users.noreply.github.com>
2024-03-14 20:15:51 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* =============typing animation============*/
+var typed = new Typed(".typing",{
+    strings:["Front-End Developer","Back End Developer","Full Stack Developer"],
+    typeSpeed:100,
+    BackSpeed:10,
+    loop:true
+})
